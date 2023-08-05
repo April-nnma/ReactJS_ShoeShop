8829e525cb993633c971dd9a8227e4931d7b9b99
Nguyen Anh <nguyenanh1904.vn@gmail.com>
2023-08-05 23:59:34 +0700

    redux

--- src/ShopShoes/ProductItem.jsx
+++ src/ShopShoes/ProductItem.jsx
@@ -1,3 +1,45 @@
+// import React from "react";
+// import { useDispatch } from "react-redux";
+// import { detailProductAction } from "./shopShoes/action";
+
+// const ProductItem = ({ shoes }) => {
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="col-md-4 py-3">
+//       <div className="card h-100 border-0 shadow-sm">
+//         <img src={shoes.image} alt="Product" className="card-img-top" />
+//         <div className="card-body">
+//           <h5 className="card-title mb-3">{shoes.name}</h5>
+//           <h6 className="card-subtitle mb-2 text-muted">
+//             {new Intl.NumberFormat("en-US", {
+//               style: "currency",
+//               currency: "USD",
+//             }).format(shoes.price)}
+//           </h6>
+//           <div className="d-flex justify-content-between align-items-center">
+//             <button className="btn btn-outline-dark rounded-pill">
+//               Add To Cart
+//             </button>
+//             <button
+//               className="btn btn-primary rounded-pill"
+//               type="button"
+//               data-bs-toggle="modal"
+//               data-bs-target="#exampleModal"
+//               onClick={() => {
+//                 dispatch(detailProductAction.handleProductDetail(shoes));
+//               }}
+//             >
+//               Details
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProductItem;
 import React from "react";
 import { useDispatch } from "react-redux";
 import { detailProductAction } from "./shopShoes/action";
@@ -18,11 +60,9 @@ const ProductItem = ({ shoes }) => {
             }).format(shoes.price)}
           </h6>
           <div className="d-flex justify-content-between align-items-center">
-            <button className="btn btn-outline-dark rounded-pill">
-              Add To Cart
-            </button>
+            <button className="custom-btn-outline">Add To Cart</button>
             <button
-              className="btn btn-primary rounded-pill"
+              className="custom-btn-details"
               type="button"
               data-bs-toggle="modal"
               data-bs-target="#exampleModal"
